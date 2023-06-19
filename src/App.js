f18b19ad58a346a789b6d922c286ccb4009cfc15
phungthanhtu <19522455@gm.uit.edu.vn>
2023-06-19 18:49:40 +0700

    navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from "./components/routes/home/home.component";
-import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/routes/navigation/navigation.component";
+import Shop from "./components/routes/shop/shop.component"
+
+import { Routes, Route} from "react-router-dom";
 
 const  App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        <Route path="shop" element={<Shop/>}/>
+      </Route>  
     </Routes>
   );
 }
--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router';
 import Directory from '../../directory/directory.component'
 
 const  Home = () => {
@@ -31,9 +32,13 @@ const  Home = () => {
   ]
   
   return (
-    <Directory 
-    categories={categories}
-    />
+    <div>
+      <Directory 
+        categories={categories}
+      />
+      <Outlet />
+    </div>
+
   );
 }
 
