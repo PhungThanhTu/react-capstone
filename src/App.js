583900690a2b5fc9f69c4a75638fb11f1e3d0851
phungthanhtu <19522455@gm.uit.edu.vn>
2023-06-21 22:16:26 +0700

    navigation styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Home from "./components/routes/home/home.component";
-import Navigation from "./components/routes/navigation/navigation.component";
-import Shop from "./components/routes/shop/shop.component"
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Shop from "./routes/shop/shop.component"
 
 import { Routes, Route} from "react-router-dom";
 
-const  App = () => {
+const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation/>}>
